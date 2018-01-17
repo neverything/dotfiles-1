8398b11e7f23526d2ba9dbafc4e2b523ca2fe5a2
Silvan Hagen <neverything@gmail.com>
2018-01-17 09:38:05 +0100

    Additional aliases with functions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,3 +7,4 @@ alias composer='ssh -T git@github.com > /dev/null 2>&1 ; ssh -T git@bitbucket.or
 alias brewmaint='brew update && brew upgrade && brew cleanup && brew cask cleanup'
 alias appflowup='cd ~/.appflow/playbooks && vagrant up atlantis'
 alias appflowdown='cd ~/.appflow/playbooks && vagrant halt atlantis'
+alias presence='_presence() { cd ~/Documents/webdev/development/$1; }; _presence'
